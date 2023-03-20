baa1b2f1f7b7c200df6a4144883d36c706e782be
bnb70 <bensonw830@gmail.com>
2023-03-20 17:02:48 +0800

    V0.4.4

--- rpi4/rpi4_ros2_ws/src/AutoFollowCar/AutoFollowCar/operation.py
+++ rpi4/rpi4_ros2_ws/src/AutoFollowCar/AutoFollowCar/operation.py
@@ -1,28 +1,76 @@
+import random
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 
+
+class coordinate_to_car():
+
+    def __init__(self):
+        self.i = 0
+        self.coordinate = []
+        self.center_point = []
+        self.move = ''
+
+    def run(self, data):
+        self.str_split(data)
+        self.get_center()
+        return self.move_state()
+
+    def random_xxyy(self):
+        x1 = random.randint(0, 640 - 10)
+        y1 = random.randint(10, 640)
+        x2 = random.randint(x1 + 10, 640)
+        y2 = random.randint(0, y1 - 10)
+        xxyy = [x1, y1, x2, y2]
+        return xxyy
+
+    def str_split(self, data=""):
+        data = data.split(":")
+        data = [data[0]] + data[1].split(",")
+        self.coordinate = data
+
+    def get_center(self):
+        center_x = abs(((self.coordinate[1] - self.coordinate[3]) / 2) + self.coordinate[1])
+        center_y = abs(((self.coordinate[2] - self.coordinate[4]) / 2) + self.coordinate[4])
+        self.center_point = [center_x, center_y]
+
+    def move_state(self):
+        if self.center_point[0] < 310:
+            move_R_L = 'L'
+        elif self.center_point[0] > 525:
+            move_R_L = 'R'
+        else:
+            move_R_L = 'N'
+        if self.center_point[1] > 235:
+            move_or_stop = 'move'
+        else:
+            move_or_stop = 'stop'
+        self.move = f'{move_or_stop}_{move_R_L}'
+        return self.move
+
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('operation')
+        self.subscription = self.create_subscription(String, 'topic_jetson_info', self.listener_callback, 10)
         self.publisher_ = self.create_publisher(String, 'topic_car_move', 10)
-        self.subscription = self.create_subscription(String,'topic_jetson_info',self.listener_callback,10)
         timer_period = 0.2  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.car_move = coordinate_to_car()
 
     def timer_callback(self):
-        msg = String()
-        msg.data = f'Send_car_move_data:{self.i}'
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Send_car_move_data:{self.i}')
+        car_move_ = self.car_move.run()
+        car_move_mag = String()
+        car_move_mag.data = f'{car_move_}'
+        self.publisher_.publish(car_move_mag)
+        self.get_logger().info(f'Send_car_move_data:{car_move_}')
         self.i += 1
 
     def listener_callback(self, jetson_msg):
         self.get_logger().info(f'To_Operation_data:{jetson_msg.data}')
-
 
 def main(args=None):
     rclpy.init(args=args)
--- rpi4/rpi4_ros2_ws/src/AutoFollowCar/AutoFollowCar/test.py
+++ rpi4/rpi4_ros2_ws/src/AutoFollowCar/AutoFollowCar/test.py
@@ -1,12 +1,18 @@
 import random
-import time
 
 
 class coordinate_to_car():
 
     def __init__(self):
-        self.coordinate = []
         self.i = 0
+        self.coordinate = []
+        self.center_point = []
+        self.move = ''
+
+    def run(self,data):
+        self.str_split(data)
+        self.get_center()
+        return self.move_state()
 
     def random_xxyy(self):
         x1 = random.randint(0, 640 - 10)
@@ -15,32 +21,27 @@ class coordinate_to_car():
         y2 = random.randint(0, y1 - 10)
         xxyy = [x1, y1, x2, y2]
         return xxyy
+
     def str_split(self,data=""):
-        self.i = len(self.coordinate)
         data = data.split(":")
         data = [data[0]] + data[1].split(",")
-        self.coordinate += [data]
-        if self.i >= 10:
-            self.coordinate.pop(0)
-        return self.coordinate
+        self.coordinate = data
 
-    def center(self):
-        self.coordinate[]
+    def get_center(self):
+        center_x = abs(((self.coordinate[1] - self.coordinate[3]) / 2) + self.coordinate[1])
+        center_y = abs(((self.coordinate[2] - self.coordinate[4]) / 2) + self.coordinate[4])
+        self.center_point = [center_x,center_y]
 
-
-
-
-
-
-
-
-
-
-a = coordinate_to_car()
-
-for i in range(100):
-    xxyy = a.random_xxyy()
-    data = f'NO.{random.randint(1,10)}:{xxyy[0]},{xxyy[1]},{xxyy[2]},{xxyy[3]}'
-    b = a.str_split(data)
-    print(b)
-    time.sleep(1)
+    def move_state(self):
+        if self.center_point[0] < 310:
+            move_R_L = 'L'
+        elif self.center_point[0] > 525:
+            move_R_L = 'R'
+        else:
+            move_R_L = 'N'
+        if self.center_point[1] > 235:
+            move_or_stop = 'move'
+        else:
+            move_or_stop = 'stop'
+        self.move = f'{move_or_stop}_{move_R_L}'
+        return self.move
